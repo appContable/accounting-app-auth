32c0a4f74f68ff63f89320c754a0829dfc6ff3f4
App Contable <appcontable2025@gmail.com>
2025-08-25 16:45:45 -0300

    Initialize Role properties

--- AccountCore.DAL/Auth/Role.cs
+++ AccountCore.DAL/Auth/Role.cs
@@ -6,11 +6,11 @@ namespace AccountCore.DAL.Auth.Models
 {
     public partial class Role
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = null!;
 
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
 
-        public string RoleKey { get; set; }
+        public string RoleKey { get; set; } = null!;
 
         public bool IsEnabled { get; set; } = true;
     }
