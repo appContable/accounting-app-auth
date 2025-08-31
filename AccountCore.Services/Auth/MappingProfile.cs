6d81ec52675bcb9b51c268f22482cea0df37a1bd
App Contable <appcontable2025@gmail.com>
2025-08-31 13:21:02 -0300

    Fix AutoMapper expression tree errors

--- AccountCore.Services/Auth/MappingProfile.cs
+++ AccountCore.Services/Auth/MappingProfile.cs
@@ -10,13 +10,13 @@ namespace AccountCore.Services.Auth
         public MappingProfile()
         {
             CreateMap<User, UserDTO>()
-                .ForMember(d => d.Roles, map => map.MapFrom(src => src.Roles?.Where(r => r.Enable) ?? new List<RoleUser>()));
+                .ForMember(d => d.Roles, map => map.MapFrom(src => src.Roles != null ? src.Roles.Where(r => r.Enable) : new List<RoleUser>()));
 
             CreateMap<RoleUser, RoleUserDTO>().ReverseMap();
             CreateMap<Role, RoleDTO>().ReverseMap();
 
             CreateMap<User, UserPostDTO>()
-                .ForMember(u => u.RoleIds, map => map.MapFrom(src => src.Roles?.Select(r => r.RoleId).ToArray() ?? Array.Empty<string>()));
+                .ForMember(u => u.RoleIds, map => map.MapFrom(src => src.Roles != null ? src.Roles.Select(r => r.RoleId).ToArray() : Array.Empty<string>()));
             CreateMap<UserPostDTO, User>();
 
         }
